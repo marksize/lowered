202cc3f5034dbaacf28340743c3e6523148b6005
test@test.com <test@test.com>
2015-10-02 21:52:16 +0200

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define babesandnerd-qt message queue name
+#define BITCOINURI_QUEUE_NAME "babesandnerdURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
